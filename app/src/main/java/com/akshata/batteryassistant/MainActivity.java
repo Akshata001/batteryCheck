0cf0af02d2e0307b8b23ad771e566dec16269144
Akshata001 <patilakshata001@gmail.com>
2018-04-24 19:26:36 +0530

    added notification and text  to speech

--- app/src/main/java/com/akshata/batteryassistant/MainActivity.java
+++ app/src/main/java/com/akshata/batteryassistant/MainActivity.java
@@ -1,5 +1,7 @@
 package com.akshata.batteryassistant;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
@@ -8,11 +10,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+  private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
+    @Override
+    public void onReceive(Context ctxt, Intent intent) {
+      int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+      Log.d(MainActivity.class.getSimpleName(), "onReceive: " + String.valueOf(level) + "%");
+    }
+  };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
+    PowerConnectionReceiver powerConnectionReceiver = new PowerConnectionReceiver(MainActivity.this);
 
     /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
@@ -30,8 +42,9 @@ public class MainActivity extends AppCompatActivity {
       }
     });*/
 
-    IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-    Intent batteryStatus = registerReceiver(null, ifilter);
+//    IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+//    Intent batteryStatus = registerReceiver(null, ifilter);
+    this.registerReceiver(powerConnectionReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
 
    /* int status = 0;
     if (batteryStatus != null) {
--- app/src/main/java/com/akshata/batteryassistant/PowerConnectionReceiver.java
+++ app/src/main/java/com/akshata/batteryassistant/PowerConnectionReceiver.java
@@ -4,7 +4,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.BatteryManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
+import com.akshata.batteryassistant.TextToSpeechHandler.TTSCompletionListener;
 import org.apache.log4j.jmx.LoggerDynamicMBean;
 
 /**
@@ -13,20 +16,44 @@ import org.apache.log4j.jmx.LoggerDynamicMBean;
 
 public class PowerConnectionReceiver extends BroadcastReceiver {
 
+  private final Context mContext;
+  private TextToSpeechHandler textToSpeechHandler;
+
+  public PowerConnectionReceiver(Context mContext) {
+    this.mContext = mContext;
+  }
+
   @Override
   public void onReceive(Context context, Intent intent) {
     int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
     boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
         status == BatteryManager.BATTERY_STATUS_FULL;
 
-    int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
-//    boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
-//    boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+    textToSpeechHandler = new TextToSpeechHandler(mContext);
+
     if (isCharging) {
       int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-      int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-      float batteryPct = level / (float) scale;
-      Log.d(PowerConnectionReceiver.class.getSimpleName(), "onReceive: batteryPct =" + batteryPct);
+      Log.d(PowerConnectionReceiver.class.getSimpleName(),
+          "onReceive: batteryPct =" + String.valueOf(level) + "%");
+
+      if (level == 94) {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(mContext)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Hey, Akshata")
+            .setContentText("Please Remove your charger")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(mContext);
+
+// notificationId is a unique int for each notification that you must define
+        int notificationId = 99999;
+        notificationManager.notify(notificationId, mBuilder.build());
+        textToSpeechHandler
+            .playMessage("Your phone battery is fully charged", new TTSCompletionListener() {
+              @Override
+              public void onTTSPlatCompleted() {
+              }
+            });
+      }
     }
   }
 }
